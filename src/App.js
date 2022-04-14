8f64a9f2f053c1fd623528317c2c9fa355ce7072
Jeremy Richardson <jrichardson@thought-dev.com>
2022-04-14 10:36:09 -0500

    feat: first round

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Typography, Grid, Avatar, Box } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container sx={{ bgcolor: "#cfe8fc", minHeight: "100vh" }} maxWidth="md">
+      <Box sx={{ padding: "3em 0" }}>
+        <Grid container>
+          <Grid item xs={12} sm={6}>
+            <Typography align="center" variant="h2" component="h1">
+              Jeremy Richardson
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={6} align="center">
+            <Avatar
+              alt="Jeremy Richardson"
+              src="/img/jeremy-profile@2x.png"
+              sx={{ width: "200px", height: "200px", background: "white" }}
+            />
+          </Grid>
+        </Grid>
+      </Box>
+      <Typography align="center" variant="h4" component="h2">
+        I'm a web developer from Iowa
+      </Typography>
+      <Typography>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+        rhoncus nulla sed orci laoreet pharetra. Suspendisse vitae eros
+        tristique, tincidunt nisl eget, viverra justo. Aliquam vitae velit
+        facilisis, dapibus est vitae, consequat tellus. Curabitur id luctus
+        magna. Aenean tempus nec massa id porta. Donec pretium vitae leo a
+        malesuada. Proin vel egestas mauris, interdum pretium eros. Suspendisse
+        faucibus laoreet lectus, eget scelerisque dolor tempor ut. Praesent
+        varius metus tellus, quis scelerisque nisl eleifend sit amet. Integer id
+        rhoncus metus. Aliquam a ipsum quis dui consectetur luctus. Ut sit amet
+        lobortis sem. Orci varius natoque penatibus et magnis dis parturient
+        montes, nascetur ridiculus mus. Nunc condimentum dui at augue
+        consectetur, eu vehicula risus commodo. Suspendisse erat diam, sagittis
+        eget convallis quis, iaculis et tellus. Morbi pellentesque massa ut
+        laoreet aliquam.
+      </Typography>
+    </Container>
   );
 }
 
